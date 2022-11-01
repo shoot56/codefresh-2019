51f22bf87c729f5d8ef89026faf4f8975ff66bf3
Dmitry Shutko <shoot56@iMac-Dmitry.lan>
2022-11-01 16:39:04 +0200

    GROW-828 add images

--- resources/views/partials/new/featured-case-studies-ticker-static.blade.php
+++ resources/views/partials/new/featured-case-studies-ticker-static.blade.php
@@ -8,8 +8,8 @@
         <img src="/wp-content/themes/codefresh-2019/dist/images/logos/mntn-color.svg" class="max-h-10 max-w-8 my-2 mx-4 ls-is-cached lazyloaded" alt="Mntn" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/mntn-color.svg">
         <img src="/wp-content/themes/codefresh-2019/dist/images/logos/tbs-1.svg" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="TBS" loading="lazy"  data-src="/wp-content/themes/codefresh-2019/dist/images/logos/tbs-1.svg">
         <img src="/wp-content/themes/codefresh-2019/dist/images/logos/goodpx.svg" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="GoodPx" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/goodpx.svg">
-        <img  src="/wp-content/themes/codefresh-2019/dist/images/logos/eight.png" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="Eight" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/eight.png">
-        <img  src="/wp-content/themes/codefresh-2019/dist/images/logos/parkhub.png" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="Parkhub" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/parkhub.png">
+        <img  src="/wp-content/themes/codefresh-2019/dist/images/logos/Eight_Sleep_black.svg" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="Eight" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/eight.png">
+        <img  src="/wp-content/themes/codefresh-2019/dist/images/logos/parkhub_orange.svg" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="Parkhub" loading="lazy" data-src="/wp-content/themes/codefresh-2019/dist/images/logos/parkhub.png">
         <img src="/wp-content/themes/codefresh-2019/dist/images/logos/monday.svg" class="max-h-10 max-w-8 my-2 mx-4 lazyloaded" alt="Monday.com" loading="lazy"  data-src="/wp-content/themes/codefresh-2019/dist/images/logos/monday.svg">
       </div>
     </div>
