cea835a47eb6406f1e8918d7f4540120d299746e
Dmitry Shutko <shoot56@iMac-Dmitry.lan>
2022-11-01 13:59:28 +0200

    GROW-842 add logos images size

--- resources/views/partials/new/featured-case-studies-static-logos.blade.php
+++ resources/views/partials/new/featured-case-studies-static-logos.blade.php
@@ -1,15 +1,15 @@
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/sama-logo.svg" class="inline-block max-h-6" alt="Sama" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/sama-logo.svg" width="86" height="24" class="inline-block max-h-6" alt="Sama" loading="lazy">
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/hover.svg" class="inline-block max-h-6" alt="Hover" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/hover.svg" width="99" height="24" class="inline-block max-h-6" alt="Hover" loading="lazy">
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/skillshare.svg" class="inline-block max-h-6" alt="Skillshare" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/skillshare.svg" width="49" height="24" class="inline-block max-h-6" alt="Skillshare" loading="lazy">
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/mntn-color.svg" class="inline-block max-h-6" alt="Mntn" loading="lazy" >
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/mntn-color.svg" width="71" height="24" class="inline-block max-h-6" alt="Mntn" loading="lazy" >
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/tbs-1.svg" class="inline-block max-h-6" alt="TBS" loading="lazy" >
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/tbs-1.svg" width="40" height="24" class="inline-block max-h-6" alt="TBS" loading="lazy" >
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/goodpx.svg" class="inline-block max-h-6" alt="GoodRx" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/goodpx.svg" width="86" height="24" class="inline-block max-h-6" alt="GoodRx" loading="lazy">
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/Parkhub-1024x192-min-1-300x56.png" class="inline-block max-h-6" alt="Parkhub" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/Parkhub-1024x192-min-1-300x56.png" width="129" height="24" class="inline-block max-h-6" alt="Parkhub" loading="lazy">
 
-<img src="/wp-content/themes/codefresh-2019/dist/images/logos/monday.svg" class="inline-block max-h-6" alt="Monday.com" loading="lazy">
+<img src="/wp-content/themes/codefresh-2019/dist/images/logos/monday.svg" width="127" height="24" class="inline-block max-h-6" alt="Monday.com" loading="lazy">
