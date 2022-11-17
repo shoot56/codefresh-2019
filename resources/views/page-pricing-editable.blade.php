5262feee6c312d37c776eece5ce096e03b80c321
Dmitry Shutko <shoot56@iMac-Dmitry.lan>
2022-11-17 16:31:01 +0200

    fix changes

---
+++ resources/views/page-pricing-editable.blade.php
@@ -0,0 +1,28 @@
+{{--
+  Template Name: Pricing Editable Template
+--}}
+
+@extends('layouts.containerless', ['bodyClass' => 'bg-nk-silver'])
+
+@section('content')
+  <div class="bg-gradient-to-b from-nk-blue-marine to-nk-blue-midnight has-separator relative">
+    
+  </div>
+  <div class="separator-container bg-nk-silver">
+    <img src="/wp-content/themes/codefresh-2019/dist/images/nk/sep-blue-midnight.svg">
+  </div>
+  <div class="wrap container">
+    
+  </div>
+  
+  <div class="separator-container bg-nk-blue-midnight">
+    <img src="/wp-content/themes/codefresh-2019/dist/images/nk/sep-silver.svg">
+  </div>
+  <div class="bg-nk-blue-midnight text-white">
+    <div class="container pb-16">
+      <h2 class="text-2xl pt-2 -mb-10 text-center mx-auto">Frequently Asked Questions</h2>
+      
+    </div>
+  </div>
+  @include('partials.new.cta-banner')
+@endsection
